7453d06652917e90ff5cb80bb5e918fcc4330cad
Waseem Dahman <dwaseem@icloud.com>
2017-04-26 23:21:04 -0400

    Configure project

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import babel from 'rollup-plugin-babel'
+const info = require('./package.json');
+
+const config = {
+  entry: 'src/index.js',
+  plugins: [
+    babel({
+      babelrc: false,
+      presets: [
+        [ 'env', {
+          targets: {
+            browsers: ['last 2 versions', 'safari >= 7']
+          },
+          'modules': false
+        }]
+      ]
+    })
+  ],
+  targets: [{
+    dest: info.main,
+    format: 'umd',
+    moduleName: 'sleepsort-promise'
+  }, {
+    dest: info.module,
+    format: 'es'
+  }]
+}
+
+export default config
