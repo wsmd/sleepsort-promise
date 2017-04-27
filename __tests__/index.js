c7d331d286c2a53392eb11523a0ad2596136bc4f
Waseem Dahman <dwaseem@icloud.com>
2017-04-26 23:22:11 -0400

    Add tests

---
+++ __tests__/index.js
@@ -0,0 +1,38 @@
+import sleepSort from '../src';
+
+const NUMBERS = [3, 10, 6, 7, 100, 4, 25];
+const SORTED = [ 3, 4, 6, 7, 10, 25, 100 ];
+
+const PROMISE = window.Promise;
+
+describe('sleepsort', () => {
+
+  it('sorts numbers using a promise', () => {
+    return sleepSort(NUMBERS).then((numbers) => {
+      expect(SORTED).toEqual(numbers);
+    });
+  });
+
+  it('sorts numbers using a callback', (done) => {
+    window.Promise = undefined; // overwriting promise
+
+    function callback(numbers) {
+      expect(SORTED).toEqual(numbers);
+      window.Promise = PROMISE; // restore promise
+      done();
+    }
+
+    sleepSort(NUMBERS, callback);
+  });
+
+  it('throws when not providing a callback', () => {
+    window.Promise = undefined; // overwriting promise
+    
+    function sortThatThrows() {
+      sleepSort(NUMBERS);
+      window.Promise = PROMISE; // restore promise
+    }
+
+    expect(sortThatThrows).toThrowError(Error)
+  })
+})
